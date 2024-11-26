ee18a0a38f0ed5eb805055a172ec715a838f5a75
L-P-J <angel20000501@gmail.com>
2024-11-26 23:02:06 +0800

    Initial commit with Next.js project

---
+++ nextjs-dashboard/app/ui/font.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+
+export const lusitana = Lusitana({
+    weight:['400','700'],
+    // [正常字重, 加粗字重]
+    subsets: ['latin']})
